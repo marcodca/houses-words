62661a18506f67568758563dd0a0680f36e0161f
marco_dca <marcodecara@gmail.com>
2019-08-18 21:48:35 +0200

    initial clean-up and setup

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,46 +1,41 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { createGlobalStyle } from "styled-components"
 
-import Header from "./header"
-import "./layout.css"
+const GlobalStyle = createGlobalStyle`
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  html {
+    text-rendering: optimizeLegibility;
+    overflow-x: hidden;
+    box-sizing: border-box;
+    -ms-overflow-style: scrollbar;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html, body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  body{
+    position: absolute;
+  }
+  a {
+    text-decoration: none;
+  }
+`
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <GlobalStyle/>
+      <main>{children}</main>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,46 @@
 import React from "react"
-import { Link } from "gatsby"
-
+// import { Link } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+import styled from "styled-components"
+
+import blackPaper from "../images/black-paper.png"
+
+const Background = styled.div`
+  position: relative;
+  background: rgb(125, 123, 114);
+  background: url(${blackPaper}) repeat;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: rgb(45, 42, 39);
+    background: linear-gradient(
+      180deg,
+      rgba(45, 42, 39, 1) 0%,
+      rgba(133, 88, 79, 0.7091211484593838) 50%,
+      rgba(24, 17, 17, 1) 100%
+    );
+    opacity: 0.8;
+    z-index: -5;
+  }
+`
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Background>
+      <h1 style={{ margin: 0 }}>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+    </Background>
   </Layout>
 )
 
