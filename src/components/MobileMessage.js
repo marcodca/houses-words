34744cd39987bb81f32fef0ac93ea041d29ecb03
marco_dca <marcodecara@gmail.com>
2019-08-22 10:00:11 +0200

    mobile message changed to media queries

--- src/components/MobileMessage.js
+++ src/components/MobileMessage.js
@@ -1,36 +1,31 @@
-// import React, { useState } from "react";
-// import styled from "styled-components"
+import React from "react"
+import styled from "styled-components"
 
-// const Message = styled.div`
-//   width: 100%;
-//   height: 100%;
-//   position: absolute;
-//   top: 0;
-//   left: 0;
-//   background: black;
-//   z-index: 10;
-//   color: white;
-//   padding: 5%;
-//   text-align: center;
-// `
+const Message = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: black;
+  z-index: 10;
+  color: white;
+  padding: 5%;
+  text-align: center;
+  @media (min-width: 768px) {
+    display: none;
+  }
+`
 
-// const MobileMessage = () => {
-//   const [isMobile, setIsMobile] = useState(false)
+const MobileMessage = () => {
+  return (
+    <Message>
+      <h1>
+        Apologies but this web is available in desktop only. The responsive
+        design is dark and full of terrors.
+      </h1>
+    </Message>
+  )
+}
 
-//   window.addEventListener("resize", () => {
-//     window.innerWidth <= 768 ? setIsMobile(true) : setIsMobile(false)
-//   })
-
-//   return (
-//     isMobile && (
-//       <Message>
-//         <h1 style={{ color: "white" }}>
-//          Apologies but this web is available in desktop only. The responsive
-//           design is dark and full of terrors.
-//         </h1>
-//       </Message>
-//     )
-//   )
-// }
-
-// export default MobileMessage
+export default MobileMessage
