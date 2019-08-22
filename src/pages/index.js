8b1bc6fd813f7e8f5453939395a0ae90b4ceb600
marco_dca <marcodecara@gmail.com>
2019-08-22 10:57:33 +0200

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ const BannersContainer = styled.div`
 const LoadingScreen = styled.div`
   width: 100%;
   height: 100%;
-  background: red;
+  background: blue;
   position: absolute;
   top: 0;
   left: 0;
